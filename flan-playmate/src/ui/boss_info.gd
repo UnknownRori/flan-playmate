1a4605191dcec05125e4db7861b69d84c894201f
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2026-03-13 06:18:14 +0700

    add: animation for enemy hp bar

--- flan-playmate/src/ui/boss_info.gd
+++ flan-playmate/src/ui/boss_info.gd
@@ -1,13 +1,20 @@
 extends VBoxContainer
 
 @onready var progress: ProgressBar = $MarginContainer/ProgressBar
+var tween: Tween
 
 func _ready() -> void:
+    
     GameState.boss_hp.hp_change.connect(_hp_change)
     progress.max_value = GameState.boss_hp.max_hp
     progress.value = GameState.boss_hp.current_hp
 
 func _hp_change(new_value: int):
-    print(new_value)
+    if tween:
+        tween.kill()
+    tween = get_tree().create_tween()
+    tween.set_trans(Tween.TRANS_SINE)
+    tween.set_ease(Tween.EASE_IN_OUT)
+    tween.tween_property(progress, "value", float(new_value), 0.1)
+    tween.play()
     progress.max_value = float(GameState.boss_hp.max_hp)
-    progress.value = float(new_value)
